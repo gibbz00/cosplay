e460b808059d93f386a1c56b23e134cae5dec76a
gibbz00 <gabrielhansson00@gmail.com>
2026-07-31 10:45:01 +0200

    chore: project setup

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    parts = {
+      url = "github:gibbz00/parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
+  };
+
+  outputs =
+    inputs@{ nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+
+      imports = [
+        inputs.parts.flakeModule.pre-commit
+        inputs.parts.flakeModule.rust
+      ];
+
+      perSystem =
+        { config, pkgs, ... }:
+        {
+          craneLibSrcPath = ./.;
+
+          devShells.default = pkgs.mkShell {
+            name = "async-wayland";
+
+            inputsFrom = [
+              config.devShells.pre-commit
+              config.devShells.rust
+            ];
+
+            packages = with pkgs; [ ];
+          };
+        };
+    };
+}
