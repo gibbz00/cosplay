4546a152dc41d662b3df93e68c8409cb47d3872b
gibbz00 <gabrielhansson00@gmail.com>
2026-08-08 21:49:43 +0200

    docs(xml): add some notes about invariant checking

--- crates/xml/src/lib.rs
+++ crates/xml/src/lib.rs
@@ -1,10 +1,22 @@
 //! # `async-wayland-xml` - Items for deserializing Wayland XML protocol declarations.
 //!
-//! Intended to be usable by multiple implementations of wayland protocol code generators.
+//! Intended to be usable by different implementations of wayland protocol code generators, with
+//! [`Protocol::from_xml`] being the starting point.
 //!
-//! Based on `serde` and `quick-xml`. Invoke the combination of both with [`Protocol::from_xml`].
+//! ### Correctness
 //!
-//! Most item and field documentation has been adapted from <https://wayland.freedesktop.org/docs/book/Message_XML.html>.
+//! The deserializer conforms to most conventions described <https://wayland.freedesktop.org/docs/book/Message_XML.html>.
+//!
+//! Exceptions in validity checking revolve around asserting "cross element" invariants. For
+//! example:
+//!
+//! * Uniqueness checking of [`Cname`]s. Be it within interfaces, requests, or arguments etc.
+//!
+//! * "since" and "deprecated since" values are not verified against the corresponding interface
+//!   version.
+//!
+//! * An i32 argument type referencing an enum is not verified to not be pointing to a bitfield enum
+//!   declaration.
 
 mod protocol;
 pub use protocol::Protocol;
