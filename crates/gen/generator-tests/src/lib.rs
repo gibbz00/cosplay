c530104ef3c3f049f90a0fc1edbe5b2bb0f408ff
gibbz00 <gabrielhansson00@gmail.com>
2026-08-13 08:37:46 +0200

    test(generator): opcode generation

--- crates/gen/generator-tests/src/lib.rs
+++ crates/gen/generator-tests/src/lib.rs
@@ -4,10 +4,22 @@ include!(concat!(env!("OUT_DIR"), "/combined.rs"));
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use async_wayland_codec::Message;
 
     #[test]
     fn rename() {
-        let _ = rename::Format { format: rename::PixelFormat::A };
+        use super::rename::*;
+        // NB: Pixelformat would normally be called Format without the rename,
+        // which would then cause a compiler error.
+        let _ = Format { format: PixelFormat::A };
+    }
+
+    #[test]
+    fn opcodes() {
+        use super::opcodes::*;
+
+        assert_eq!(0, ReqA::OP_CODE);
+        assert_eq!(1, ReqB::OP_CODE);
+        assert_eq!(0, EvC::OP_CODE);
     }
 }
