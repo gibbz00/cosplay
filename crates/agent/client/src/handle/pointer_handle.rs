e41ff4341635d091c35495cf85eb795bd8f31e06
gibbz00 <gabrielhansson00@gmail.com>
2026-08-21 20:29:02 +0200

    feat(client): `WlPointerHandle` drop sends release request

--- crates/agent/client/src/handle/pointer_handle.rs
+++ crates/agent/client/src/handle/pointer_handle.rs
@@ -1,4 +1,4 @@
-use cosplay_protocols_wayland::wl_pointer::WlPointer;
+use cosplay_protocols_wayland::wl_pointer::{self, WlPointer};
 
 use crate::*;
 
@@ -10,5 +10,25 @@ pub struct WlPointerHandle {
 impl WlPointerHandle {
     pub(super) fn new(object_handle: ObjectHandle<WlPointer>) -> Self {
         Self { object_handle }
+    }
+}
+
+impl Drop for WlPointerHandle {
+    fn drop(&mut self) {
+        let _ = self.object_handle.queue_request(wl_pointer::Release);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn drops_sends_release() {
+        let (mut test_driver, object_handle) = TestDriver::new();
+
+        let pointer_handle = WlPointerHandle::new(object_handle);
+
+        test_driver.assert_queued_destructor_on_drop::<wl_pointer::Release, _>(pointer_handle.object_handle.id, pointer_handle);
     }
 }
